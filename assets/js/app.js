0cabac12e61c12472acdf5f5b1a0aabdfb4e0d76
Ryan Tablada <ryan.tablada@gmail.com>
2015-07-28 12:13:50 -0500

    Updated to use filter to add live searching

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,31 @@
 var target = $('.target');
 var templateStr = $('#podcast-template').html();
-var compileTemplate = Handlebars.compile(templateStr);
+var searchBar = $('.search-bar');
+var allResults;
 
+var compileTemplate = Handlebars.compile(templateStr);
 var apiUrl = 'https://calm-reaches-2019.herokuapp.com/podcasts';
+
+var render = function(output) {
+  var results = compileTemplate(output);
+  target.html(results);
+};
 
 $.ajax({
   url: apiUrl,
-  success: function(output) {
-    var results = compileTemplate(output);
-    target.html(results);
+  success: function(data) {
+    render(data);
+    allResults = data;
   }
+});
+
+searchBar.on('input', function() {
+  var inputStr = searchBar.val().toLowerCase();
+
+  var filteredResults = allResults.filter(function(current) {
+    return current.title.toLowerCase().indexOf(inputStr) > -1;
+  });
+
+  // Filter down our results
+  render(filteredResults);
 });
